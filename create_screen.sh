5f31b6beb83115933e9d56c04515b7e27787b554
henry perlman <bperlman1@gmail.com>
2023-02-09 09:22:21 -0500

    add .sh commands to create bash screens when executing on ubuntu servers

--- create_screen.sh
+++ create_screen.sh
@@ -1,3 +1,3 @@
 screen -S wordle_py_server -dm bash -c "bash uvicorn_run.sh"
 sleep 5
-screen -S wordle_http_server -dm bash -c "bash http_server.sh"
+screen -S wordle_http_server -dm bash -c "nvm use 16.13.1;bash http_server.sh"
